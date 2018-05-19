f056ee8adcd0dc3f47523a79255a4add57bab823
Unknown <masakudamatsu@gmail.com>
2018-05-19 18:12:31 +0900

    Copy and paste the template code

---
+++ resources/js/main.js
@@ -0,0 +1,14 @@
+var object = {};
+const = ;
+
+// jQuery
+$(document).ready(function() {
+  // Here write event handers
+});
+
+// Define functions
+object.Function1 = function() {
+};
+
+object.Function2 = function() {
+};
